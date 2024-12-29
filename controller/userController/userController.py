d8e2a9693fea5d52626c743b5c086166905de9cf
IAmAreza <ahmadrezacuet.cse@gmail.com>
2024-12-29 21:56:19 +0600

    Completed CRUD OPS

--- controller/userController/userController.py
+++ controller/userController/userController.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-
+from flask import request
 # Create a Blueprint instance
 user_controller = Blueprint('user_controller', __name__)
 
@@ -7,6 +7,31 @@ from models.usermodel.usermodel import user_model
 
 obj = user_model() 
 
+# this is for get all user 
 @user_controller.route('/users')
 def user_homepage():
     return obj.user_signup(); 
+
+#creating user 
+@user_controller.route('/users/adduser',methods=['POST']) 
+
+def adduser(): 
+    newdata = request.form    
+    return obj.adduser(newdata);   
+
+#updating user 
+
+@user_controller.route('/users/updateuser',methods=['PUT']) 
+
+def updateuser():
+    newdata = request.form  
+    
+    return obj.updateuser(newdata); 
+ 
+#deleting user 
+
+@user_controller.route('/users/deleteuser/<id>',methods=['DELETE']) 
+
+def deleteuser(id):
+
+    return obj.deleteuser(id);
--- models/usermodel/usermodel.py
+++ models/usermodel/usermodel.py
@@ -8,7 +8,8 @@ class user_model():
                                   database='user_db',user='root', 
                                   password='root')
       print('Database connection established') 
-      self.cursor = self.con.cursor(dictionary=True) 
+      self.cursor = self.con.cursor(dictionary=True)  
+      self.con.autocommit = True ## very very crucial line as it is responsible for the auto commit of the database. without this line, the database will not be updated.
     except:
       print('Database connection error')
     
@@ -17,4 +18,50 @@ class user_model():
     self.cursor.execute('SELECT * FROM users') 
     result = self.cursor.fetchall() 
     result = json.dumps(result)  
-    return result  
+    return result   
+  
+
+  def adduser(self, data):
+    print(data)
+    # Corrected SQL query and parameter passing
+    query = '''
+        INSERT INTO users (name, email, phone, password, role) 
+        VALUES (%s, %s, %s, %s, %s)
+    '''
+    # Explicitly converting data['name'] to string
+    self.cursor.execute(query, (
+        str(data['name']),       # Convert to string
+        data['email'], 
+        data['phone'], 
+        data['password'], 
+        data['role']
+    ))
+    return "User added successfully, hurrah!!"
+  
+  def updateuser(self, data):
+    print(data); 
+    query = '''
+        UPDATE users
+        SET name = %s, email = %s, phone = %s, password = %s, role = %s
+        WHERE id = %s
+    '''
+    self.cursor.execute(query, (
+        str(data['name']),       # Convert name to string (if necessary)
+        str(data['email']),      # Convert email to string (if necessary)
+        str(data['phone']),      # Convert phone to string (if necessary)
+        str(data['password']),   # Convert password to string (if necessary)
+        str(data['role']),       # Convert role to string (if necessary)
+        data['id']          # user_id as an integer (matching the 'id' column in users table)
+    ))
+    return "User updated successfully!"
+  
+
+  def deleteuser(self, id):
+    print(id)
+    query = '''
+        DELETE FROM users
+        WHERE id = %s
+    '''
+    self.cursor.execute(query, (id,))
+    return "User deleted successfully!"
+
