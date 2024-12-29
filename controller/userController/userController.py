db6751a3adc6c5ef44bfbb1cede4166b774c40df
IAmAreza <ahmadrezacuet.cse@gmail.com>
2024-12-29 18:50:45 +0600

    user model is added

--- controller/userController/userController.py
+++ controller/userController/userController.py
@@ -3,6 +3,10 @@ from flask import Blueprint
 # Create a Blueprint instance
 user_controller = Blueprint('user_controller', __name__)
 
+from models.usermodel.usermodel import user_model 
+
+obj = user_model() 
+
 @user_controller.route('/users')
 def user_homepage():
-    return 'This is the users homepage'
+    return obj.user_signup(); 
---
+++ models/usermodel/usermodel.py
@@ -0,0 +1,3 @@
+class user_model():
+  def user_signup(self):
+    return 'This is the user signup page' 
